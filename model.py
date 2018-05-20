49b3950aae03e87d0f23c09e6b4828a50d00de83
chanonroy <chanonroy@gmail.com>
2018-05-19 22:53:08 -0400

    added more labeling

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ from keras.layers import Dense, Activation, Dropout, Flatten
 from keras.models import Sequential
 
 
-def load_model(IMG_SIZE):
+def load_CNN_binary(IMG_SIZE):
     model = Sequential()
 
     model.add(Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
@@ -27,6 +27,36 @@ def load_model(IMG_SIZE):
 
     model.compile(
         loss='binary_crossentropy',
+        optimizer='adam',
+        metrics=['accuracy'])
+
+    return model
+
+
+def load_CNN_categorical(IMG_SIZE):
+    model = Sequential()
+
+    model.add(Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(32, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Conv2D(64, (3, 3), input_shape=(IMG_SIZE, IMG_SIZE, 3)))
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+
+    model.add(Flatten())
+    model.add(Dense(64))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1))
+    model.add(Activation('sigmoid'))
+
+    model.compile(
+        loss='categorical_crossentropy',
         optimizer='adam',
         metrics=['accuracy'])
 
--- predict.py
+++ predict.py
@@ -1,20 +1,16 @@
 from keras.models import load_model
 from keras.preprocessing.image import img_to_array, load_img
 
-IMG_SIZE = 150
+IMG_SIZE = 100
 
 model = load_model('./models/CNN.h5')
 
-img = load_img('./test/dog.jpg', target_size=(IMG_SIZE, IMG_SIZE))
+img = load_img('./test/doge.jpg', target_size=(IMG_SIZE, IMG_SIZE))
 x = img_to_array(img)
 x = x.reshape((1,) + x.shape)
 x = x/255
 
-value = model.predict(x)[0][0]
-
-if value >= 0.5:
-    print(value)
-    print('Cat')
-else:
-    print(value)
-    print('Dog')
+# dog = [1, 0]
+# cat = [0, 1]
+prediction = model.predict(x).tolist()
+print(prediction[0])
