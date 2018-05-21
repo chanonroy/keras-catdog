7c933037201491d6918a66c88e59f011a5b02963
chanonroy <chanonroy@gmail.com>
2018-05-21 16:51:35 -0400

    fined tuned on inception and vgg16

--- predict.py
+++ predict.py
@@ -3,9 +3,9 @@ from keras.preprocessing.image import img_to_array, load_img
 
 IMG_SIZE = 100
 
-model = load_model('./models/CNN.h5')
+model = load_model('./models/CNN-basic-100.h5')
 
-img = load_img('./test/doge.jpg', target_size=(IMG_SIZE, IMG_SIZE))
+img = load_img('./test/cat.jpg', target_size=(IMG_SIZE, IMG_SIZE))
 x = img_to_array(img)
 x = x.reshape((1,) + x.shape)
 x = x/255
